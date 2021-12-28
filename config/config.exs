0377846197c78a63f1e91fbdfeabafe2a1f5f9cd
siddhant3030 <ssiddhant3030@gmail.com>
2021-12-29 00:29:12 +0530

    liveview project

---
+++ config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :poeticoins, PoeticoinsWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "jAhW/bDc84UVsxT/RIuHxGAYkFBPxqt46942NwF71Qd5rls+2/tXTYLtR/s4YDBy",
+  render_errors: [view: PoeticoinsWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Poeticoins.PubSub,
+  live_view: [signing_salt: "F5FaR3Ez"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
