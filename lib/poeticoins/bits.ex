6e3755de96d47d4a18e326f40ee4fb72c44f4e9e
siddhant <ssiddhant3030@gmail.com>
2022-09-04 02:00:44 +0530

    deps added

--- lib/poeticoins/bits.ex
+++ lib/poeticoins/bits.ex
@@ -1,10 +1,10 @@
-defmodule Poeticoins.Packet do
+# defmodule Poeticoins.Packet do
 
-  def decode(<<_version::3, rest::bits>>), do: decode_type(rest)
+#   def decode(<<_version::3, rest::bits>>), do: decode_type(rest)
 
-  defp decode_type(<<_version::3, rest::bits>>), do: decode_literal(rest)
+#   defp decode_type(<<_version::3, rest::bits>>), do: decode_literal(rest)
 
-  defp decode_literal(<<1::1, part::4,rest::bits>>), do: [part | decode_literal(rest)]
+#   defp decode_literal(<<1::1, part::4,rest::bits>>), do: [part | decode_literal(rest)]
 
-  defp decode_literal(<<1::1, part::4,rest::bits>>), do: [part]
-end
+#   defp decode_literal(<<1::1, part::4,rest::bits>>), do: [part]
+# end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Poeticoins.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:gun, "~> 1.3"},
-      {:cowlib, "~> 2.9.1", override: true}
+      {:cowlib, "~> 2.9.1", override: true},
+      {:certifi, "~> 2.9"}
     ]
   end
 
